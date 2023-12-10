512ab28d214e2c52907cb649c3a367bee9218f59
Eng-Abdelrahman-Mostafa <abdelrahmanmostafa785@gmail.com>
2023-12-10 19:15:01 +0200

    backtrake fix

--- main_backtracking.py
+++ main_backtracking.py
@@ -1,39 +1,25 @@
-from app.Job import Job
+from genetic.Job import Job
 from genetic.Schedule import Schedule
+from app.config import MACHINE_CAPACITY,NUM_MACHINES
 
+# Your existing code
 jobs = [
-    # Job: C, Duration: 20, Prerequisites: ['A'], Resources: ['r1', 'r2']
-    Job('A', 50),
-    Job('B', 7),
-    Job('C', 20, ['A'], ["r1", "r2"]),
-    Job('D', 4, ['C']),
-    Job('E', 31, ['B'], ["r3"]),
+    Job('1', 40,),
+    Job('2', 50,['1']),
+    Job('3', 4,['2'], "r1"),
 ]
-new_jobs = []
 
-for job in jobs:
-    if job.is_capacity_exceeded():
-        divided_jobs = job.divide_job()
-        if divided_jobs:
-            new_jobs.extend(divided_jobs)
+max_time = MACHINE_CAPACITY * NUM_MACHINES
+total_job_time = Job.TOTAL_TIME
+if max_time < total_job_time:
+    print("Total time of jobs exceeded the available ")
+    
+# دي اللي بتظبط الجوبات
+Job.handle_split_dependencies(jobs)
 
-#add new jobs to jobs list
-jobs.extend(new_jobs)
-
-def encode_schedule(jobs):
-    # Flatten the list of jobs into a dictionary for easy access
-    job_dict = {job.name: job for job in jobs}
-
-    # Initialize an empty schedule
-    schedule = []
-
-    # Iterate through jobs and add them to the schedule in a valid order
-    for job in jobs:
-        if all(prerequisite in schedule for prerequisite in job.prerequisites):
-            schedule.append(job.name)
-
-    return schedule
-
+# دا عرض مطبوع
+print("################### New Jobs: #####################")
 for job in jobs:
     print(f"Job: {job.name}, Duration: {job.duration}, Prerequisites: {job.prerequisites}, Resources: {job.resources}")
-print("Schedule: ", encode_schedule(jobs))
+
+# هتكمل بال (jobs)
