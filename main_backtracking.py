6fa7ad4ad455b8bdeb4d009bc23b66258a4f5c15
Eng-Abdelrahman-Mostafa <abdelrahmanmostafa785@gmail.com>
2023-12-19 14:45:38 +0200

    structure modified

--- main_backtracking.py
+++ main_backtracking.py
@@ -34,37 +34,37 @@ class Backtracking:
         backtracking_algorithm.solve()
 
 
-# Define the job scheduling problem instance
-jobs = [
-    Job('1', 21),
-    Job('2', 5, requiredResource_id=2),
-    Job('3', 1, requiredResource_id=2),
-    Job('4', 4),
-    Job('5', 20, dependency='1'),
-]
+# # Define the job scheduling problem instance
+# jobs = [
+#     Job('1', 21),
+#     Job('2', 5, requiredResource_id=2),
+#     Job('3', 1, requiredResource_id=2),
+#     Job('4', 4),
+#     Job('5', 20, dependency='1'),
+# ]
 
 
 
 
-# TODO: split the large jop to two or more if needed 
-if(not Job.check_total_processing_time(jobs, resources)):
-    print("Exceeded the total resource capacity")
+# # TODO: split the large jop to two or more if needed 
+# if(not Job.check_total_processing_time(jobs, resources)):
+#     print("Exceeded the total resource capacity")
 
-[invalid_assigned_jobs_ids, resource_id] = Job.check_exceeded_require_resource_capacity(jobs, resources)
+# [invalid_assigned_jobs_ids, resource_id] = Job.check_exceeded_require_resource_capacity(jobs, resources)
 
-if(invalid_assigned_jobs_ids is not None):
-    joined_ids = ', '.join(str(job_id) for job_id in invalid_assigned_jobs_ids)
-    print(f"Warning: Total processing time for jobs {joined_ids} assigned to Resource {resource_id} exceeds its capacity.")
+# if(invalid_assigned_jobs_ids is not None):
+#     joined_ids = ', '.join(str(job_id) for job_id in invalid_assigned_jobs_ids)
+#     print(f"Warning: Total processing time for jobs {joined_ids} assigned to Resource {resource_id} exceeds its capacity.")
 
-preparedJobs = Job.check_and_split_large_jobs(jobs,resources)
+# preparedJobs = Job.check_and_split_large_jobs(jobs,resources)
 
-# for job in jobs:
-#     print(f'id: {job.job_id}, time: {job.processing_time}, depend: {job.dependency}, required: {job.requiredResource_id}')
-# for job in preparedJobs:
-#     print(f'id: {job.job_id}, time: {job.processing_time}, depend: {job.dependency}, required: {job.requiredResource_id}')
+# # for job in jobs:
+# #     print(f'id: {job.job_id}, time: {job.processing_time}, depend: {job.dependency}, required: {job.requiredResource_id}')
+# # for job in preparedJobs:
+# #     print(f'id: {job.job_id}, time: {job.processing_time}, depend: {job.dependency}, required: {job.requiredResource_id}')
 
-problem_instance = JobSchedulingProblem(preparedJobs, resources)
+# problem_instance = JobSchedulingProblem(preparedJobs, resources)
 
-# Create an instance of the BacktrackingAlgorithm and solve the problem
-backtracking_algorithm = BacktrackingAlgorithm(problem_instance)
-backtracking_algorithm.solve()
+# # Create an instance of the BacktrackingAlgorithm and solve the problem
+# backtracking_algorithm = BacktrackingAlgorithm(problem_instance)
+# backtracking_algorithm.solve()
